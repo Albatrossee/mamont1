d1ed727390b2e2c7d7076fe81e33ec0803585bd2
Albatrossee <56092033+Albatrossee@users.noreply.github.com>
2019-10-25 03:07:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import telebot
 from emoji import emojize
 from redis import StrictRedis
-r = StrictRedis(host='redis-13118.c59.eu-west-1-2.ec2.cloud.redislabs.com', port=13118, db=0)
+r = redis.from_url('redis://h:pc31dfa87bd081058099cf9822fb3f008108e69b45b00998978db346fd936b190@ec2-52-51-17-151.eu-west-1.compute.amazonaws.com:29109')
+
 
 TOKEN = '1027125386:AAGJEha1Q6NJcKn2g8XMc8gDZpDd7OOqub4'
 bot = telebot.TeleBot(TOKEN)
